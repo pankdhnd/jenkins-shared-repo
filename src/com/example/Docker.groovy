00f2e7e48e59bf3031dff98f44321f60460dd5ce
Pankaj <pank.dhnd@gmail.com>
2021-07-15 00:23:19 +0530

    Separated each function

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -6,17 +6,21 @@ class Docker implements Serializable {
         this.script = script
     }
 
-    def buildDockerImage (){
-        script.echo "Building docker image"
+    def dockerLogin () {
         script.withCredentials([
                 script.usernamePassword(credentialsId: '3703ba37-a8bc-4d6e-8ab4-4bbbf4df5e8e', usernameVariable: 'USER', passwordVariable: 'PASS')
-        ])
+        ]) {
+            script.sh 'echo $script.PASS | docker login -u $script.USER --password-stdin'
+        }
+    }
 
-                {
-                    script.sh "docker build -t pankajdh/testrepo:java-maven-app-$imageVersion ."
-                    script.sh 'echo $script.PASS | docker login -u $script.USER --password-stdin'
-                    script.sh "docker push pankajdh/testrepo:java-maven-app-$imageVersion"
-                }
+    def buildDockerImage (){
+        script.echo "Building docker image"
+        script.sh "docker build -t pankajdh/testrepo:java-maven-app-$imageVersion ."
+    }
+
+    def dockerPush () {
+        script.sh "docker push pankajdh/testrepo:java-maven-app-$imageVersion"
     }
 
 }
